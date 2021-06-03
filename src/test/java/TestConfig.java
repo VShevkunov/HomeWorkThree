302e84eaa7d1b8581ca3eb71c82dc342c204f43b
VShevkunov <slava473@mail.ru>
2021-06-03 15:56:36 +0300

    рефакторинг, упрощение

--- src/test/java/TestConfig.java
+++ src/test/java/TestConfig.java
@@ -3,19 +3,8 @@ import org.aeonbits.owner.Config;
 @Config.Sources({ "classpath:TestConfig.properties" })
 
 public interface TestConfig extends Config {
-    String hostnameOtus();
-    String hostnameTele2();
-    String expectedTitle();
-    String contactsBtn();
-    String actualAddressElement();
-    String expectedAddress();
-    String searchNumberField();
-    String FAQ();
-    String questionBtn();
-    String answerText();
-    String actualAnswer();
-    String emailForm();
-    String subscribeTextModule();
-    String expectedSubscribeText();
-    String subscibeBtn();
+    String hostnameYM();
+    String Samsung();
+    String Xiaomi();
+
 }
